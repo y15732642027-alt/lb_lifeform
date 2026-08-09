3d344b79ad4ff0505c6e34572ca60eb157903391
y15732642027-alt <y15732642027@users.noreply.github.com>
2026-08-09 15:39:51 +0800

    语音端口修复·0.1.0+9

--- lib/screens/presence_tab.dart
+++ lib/screens/presence_tab.dart
@@ -88,7 +88,7 @@ class _PresenceTabState extends State<PresenceTab> with SingleTickerProviderStat
 
     try {
       // 上传录音到语音API
-      final uri = Uri.parse('http://symbio.xin:8898/voice');
+      final uri = Uri.parse('http://symbio.xin/voice');
       final request = http.MultipartRequest('POST', uri);
       request.files.add(await http.MultipartFile.fromPath('file', _recordPath!));
       final response = await request.send().timeout(Duration(seconds: 30));
